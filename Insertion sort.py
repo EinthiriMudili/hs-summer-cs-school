0e3da6a82b0a14b0663fd3c541cc6e46e27cd85a
EinthiriMudili <66833966+EinthiriMudili@users.noreply.github.com>
2020-06-23 14:23:53 -0500

    Delete Insertion sort.py

--- Insertion sort.py
+++
@@ -1,9 +0,0 @@
-def insertion_sort(A):
-    for i in range (1, len(A)):
-        curNum = A[i]
-        for j in range (i-1, 0, -1):
-            if A[j] > curNum:
-                A[j+1] = A[j]
-            else:
-                A[j+1] = curNum
-                break
