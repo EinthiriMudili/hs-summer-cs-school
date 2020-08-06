117baf46fa794c014e6c0cb5ffe35b9db842376f
Einthiri Mudili <einthirimudili@Einthiris-MacBook-Pro.local>
2020-08-06 13:56:44 -0500

    message

---
+++ Hw8/Pagination.py
@@ -0,0 +1,50 @@
+def get_available_pages(page_number, pages, window_size):
+
+	result = [None] * window_size
+
+i = 0 
+size = 0 
+while size < window_size 
+	if page_number - i >= 0: 
+		result[page_number - i -1] = page_number - i
+		size = size + 1
+	if page_number + i -1 < window_size:
+		result[page_number + i -1]= page_number + i
+		size = size + 1
+	
+
+
+	for i in range(0, window_size):
+		result[page_number - i] = page_number - i
+		result[page_number + i] = page_number + i 
+
+
+    return [1,2,3,4,5]
+
+
+def assert_thing(expected, received):
+    if expected != received:
+        print(f"expected {expected} but received {received}")
+    else:
+        print(f"Good: {expected}")
+
+
+# How should the algorithm behave:
+#   At each page algorithm should try to have same number
+#   of pages before and after. If before or after is missing
+#   some number of pages then we need to make up for that in
+#   the opposite side
+#
+#   [1,2,3,4,5,6,7,8,9,10]
+    
+
+assert_thing(get_available_pages(1,10,5), [1,2,3,4,5])
+assert_thing(get_available_pages(2,10,5), [1,2,3,4,5])
+assert_thing(get_available_pages(3,10,5), [1,2,3,4,5])
+assert_thing(get_available_pages(4,10,5), [2,3,4,5,6])
+assert_thing(get_available_pages(5,10,5), [3,4,5,6,7])
+assert_thing(get_available_pages(6,10,5), [4,5,6,7,8])
+assert_thing(get_available_pages(7,10,5), [5,6,7,8,9])
+assert_thing(get_available_pages(8,10,5), [6,7,8,9,10])
+assert_thing(get_available_pages(9,10,5), [6,7,8,9,10])
+assert_thing(get_available_pages(10,10,5), [6,7,8,9,10])
